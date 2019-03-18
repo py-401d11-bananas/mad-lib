3e7a63a6d08177e37582f916f80f79c7fd66f952
EvyHaan <evy.haan@gmail.com>
2019-03-18 13:21:20 -0700

    set base routes

--- src/__init__.py
+++ src/__init__.py
@@ -5,10 +5,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-app = Flask(__name__, static_url_path='', static_forlder='static', instance_relative_config=True)
+app = Flask(__name__, static_url_path='', static_folder='static', instance_relative_config=True)
 
 app.config.from_mapping(
     SECRET_KEY=os.environ.get('SECRET_KEY'),
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
+
+from . import routes
---
+++ src/auth.py
@@ -0,0 +1,2 @@
+from flask import render_template, redirecturl_for, session, g
+from . import app
